693800023df3944c33f07fd017f19754a9ebd9c1
yennanliu <f339339@gmail.com>
2020-03-13 12:00:27 +0800

    add install via gem, install fluentd-ui (install_fluentd_mac.sh)

--- script/install_fluentd_mac.sh
+++ script/install_fluentd_mac.sh
@@ -1,8 +1,22 @@
 #!/bin/sh
 
-# 1) Install fluentd Mac OSX
+# 1) Install fluentd Mac OSX]
+
+# methid 1 via gem 
+# https://docs.fluentd.org/installation/install-by-gem
+gem install fluentd --no-ri --no-rdoc
+# run 
+fluentd --setup ./fluent
+fluentd -c ./fluent/fluent.conf -vv &
+echo '{"json":"message"}' | fluent-cat debug.test
+
+# method 2 : via td-agent
 # https://docs.fluentd.org/installation/install-by-dmg
 
 # 2) Install fluentd UI Mac OSX
 gem install -V fluentd-ui
 fluentd-ui start
+
+
+# 3) Stop the fluentd daemon
+pkill -f fluentd
